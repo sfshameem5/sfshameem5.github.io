5f33e439055d79b979740d334fc0a2410b438cb3
Shameem <sfshameem5@gmail.com>
2018-04-05 12:45:36 +0530

    Added typings

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var options = {
+	strings: [
+	"Web Applications", 
+	"Web Sites", 
+	"User Interfaces"
+	],
+	typeSpeed: 70,
+	loop: true,
+	backDelay: 1500,
+	backSpeed: 50
+}
+
+var typed = new Typed("#typer", options);
